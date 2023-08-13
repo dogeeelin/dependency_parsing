829bd61f5da7adb0674167c5cd8b62de0cf7b21c
YUN LIN <120708262+dogeeelin@users.noreply.github.com>
2023-08-13 16:13:20 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-CS224N 2020-2021: Homework 3
-run.py: Run the dependency parser.
-Sahil Chopra <schopra8@stanford.edu>
-Haoshen Hong <haoshen@stanford.edu>
-"""
+
 from datetime import datetime
 import os
 import pickle
